767517c15e3fa72c42b6a4adc00d83c04868ed6b
Katherine Aguirre <katherine.aguirre@kinandcarta.com>
2022-08-16 19:27:59 -0300

    chore: initial commit

---
+++ src/main/java/com/kathesama/seedcleanhexagonalarchitecture/SeedCleanHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.kathesama.seedcleanhexagonalarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedCleanHexagonalArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedCleanHexagonalArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kathesama/seedcleanhexagonalarchitecture/SeedCleanHexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kathesama.seedcleanhexagonalarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedCleanHexagonalArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
